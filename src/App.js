26075cafc754f756e0f8d785ee59d90c472d25c8
Oscar6 <oscar.m1024@gmail.com>
2019-04-15 14:02:50 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Pawrks Tracker
           </a>
         </header>
       </div>
